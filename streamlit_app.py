2fc72d48f0950af9bdaf61870c6a887a49a945f4
Minyoi <42159557+Minyoi@users.noreply.github.com>
2025-07-02 15:56:20 +0000

    adding altair library

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from pandasql import sqldf
-
+import altair as alt
 import pandas as pd
 import altair as alt
 import plotly.express as px
