47bec9840117a25c6aa9c15692d85827045bc249
Minyoi <42159557+Minyoi@users.noreply.github.com>
2025-07-10 09:17:44 +0000

    update altair chart type

--- streamlit_app.py
+++ streamlit_app.py
@@ -203,7 +203,7 @@ with col[0]:
     # Create an Altair chart
     chart = alt.Chart(culture_tats).mark_area(opacity=0.5).encode(
         x='month',
-        y='Negative,Positive,TF,Z_test,Contaminated:Q',
+        y='Negative,Positive,TF,Z_test,Contaminated:T',
         color='source:N'  # Use the category field for color encoding
     ).properties(
         title="My plot"
